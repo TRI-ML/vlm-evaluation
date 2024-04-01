9d16aabd8892fd52eb302c988e4e762d879c6fae
Matthias Buchner <matthias.buchner@gmail.com>
2024-04-01 22:52:47 +0200

    update log stmt

--- vlm_eval/models/prismatic.py
+++ vlm_eval/models/prismatic.py
@@ -61,7 +61,7 @@ class PrismaticVLM(VLM):
         vlm = load(load_from, hf_token=self.hf_token)
         vlm.to(self.distributed_state.device, dtype=self.dtype)
 
-        print(f"loaded prismatic {self}")
+        print(f"loaded prismatic {self.model_id}")
 
         # Get Tokenizer and Image Processor
         tokenizer, image_transform = vlm.llm_backbone.tokenizer, vlm.vision_backbone.image_transform
